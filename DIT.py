703bd8ec27d58ddc0e8088a2c5418fb08becc809
Jacob Wu <Jacob.wu@my.bdsc.school.nz>
2026-03-30 14:46:25 +1300

    Rock paper scissors

---
+++ DIT.py
@@ -0,0 +1,33 @@
+import random
+play = "y"
+while play == "y":
+    user = input("Rock Paper Scissors: ").lower()
+    com = random.choice(["rock", "paper", "scissors"])
+    if com == user:
+        print(f"Computer chose {user}, It is a tie")
+
+    elif user == "paper":
+        if com == "scissors":
+            print("Computer chose scissors: You lost!")
+        else:
+            print("Computer chose rock: You Win!")
+
+    elif user == "rock":
+        if com == "paper":
+            print("Computer chose paper: You lost!")
+        else:
+            print("Computer chose scissors: You Win!")
+
+    elif user == "scissors":
+        if com == "rock":
+            print("Computer chose rock: You lost!")
+        else:
+            print("Computer chose paper: You Win!")
+
+    else:
+        print("Invalid Choice")
+    play = input("Play again?(Y/N): ").lower()
+
+
+    if play == "n":
+        print("Shut off")
